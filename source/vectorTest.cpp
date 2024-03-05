2e5d5ffb053fd35e87e931f1831bbdefa7b2b9ff
Super Hackio <superhaxio@gmail.com>
2024-03-05 09:36:23 -0800

    Update vectorTest.cpp

--- source/vectorTest.cpp
+++ source/vectorTest.cpp
@@ -13,4 +13,10 @@ void VectorTest::someFunction() {
     char_Vec.negate();
     short_Vec.negate();
     float_Vec.negate();
+
+    char_Vec = -JGeometry::TVec3<char>(5, 5, 5);
+    char_Vec += char_Vec + JGeometry::TVec3<char>(5, 5, 5);
+    char_Vec -= char_Vec - JGeometry::TVec3<char>(5,5,5);
+    char_Vec *= char_Vec * 5;
+    char_Vec /= char_Vec / 2;
 }
